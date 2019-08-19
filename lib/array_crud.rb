fbfb228607dfa06abadc6ecc7f0ec30cf5063dae
Eli Kim <eliskim27@gmail.com>
2019-08-19 21:34:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,9 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = [0,1,2,3,4,5]
+  element = "wow"
+  array
 end
 
 def remove_element_from_end_of_array(array)
